0908719e349c360001f8d1f3ccfaf934b06ba92b
Matt Daalder <matt.daalder@gmail.com>
2020-02-04 15:36:43 -0700

    remove large files

--- W02_filtering/02_Assignment/Code try.py
+++ W02_filtering/02_Assignment/Code try.py
@@ -19,7 +19,7 @@ inputReader = vtk.vtkNIFTIImageReader()
 inputReader.SetFileName(imageFile)
 inputReader.Update()
 
-inputImageData = inputReader.GetOutput()
+inputImage = inputReader.GetOutput()
 
 imageName = inputReader.GetFileName()
 outfile = str(os.path.splitext(imageName)[0]) + '_GaussianSmoothed.nii'
@@ -33,29 +33,33 @@ copyImage = vtk.vtkImageData()
 copyImage.DeepCopy(inputReader.GetOutput()) 
 
 # get the image dimensions
-imageDim = inputImageData.GetDimensions()
-print(imageDim)
-imageExt = inputImageData.GetExtent()
-print(imageExt)
-print(len(imageExt))
-kernelDim = 3 # could make this an input arg in the future
+imageDim = inputImage.GetDimensions()
+print("imagedim= " + str(imageDim))
+imageExt = inputImage.GetExtent()
+print("image extents= " + str(imageExt))
+print("length of image ext= " + str(len(imageExt)))
+kernelSize = 3 # could make this an input arg in the future
 
-imageDim = np.empty(len(imageExt), dtype=int)
-# imageDimMin = np.empty(3, dtype = int)
-# imageDimMax = np.empty(3, dtype = int)
+imageDimMin = np.empty(3, dtype = int) # will have the starting coordinates for the image we want to smooth (padded)
+imageDimMax = np.empty(3, dtype = int) # will have the ending coordinates for the images we want to smooth (padded)
 
+# dummy variables
+a = 0
+b = 0
 
-for i in range(0, len(imageDim)):
-    imageDim[i] = imageExt[i] + 1
+# set up the dimenions of the image we are analyzing to help iterate over the image's data
+# accounts for the size of the smoothing kernel (i.e. implementing padding)
+for i in range(0,len(imageExt),2): # 0 to 4 by twos to get all even numbers, corresponds to x y z min coords   
+    imageDimMin[a] = imageExt[i]+(kernelSize - kernelSize//2) # creates a list of the image's min coordinates to start smoothing from
+    a += 1
+for i in range(1,(len(imageExt)+1),2): # 1 to 5 by twos to get all uneven numbers, corresponds to x y z max coords
+    imageDimMax[b] = imageExt[i]-(kernelSize - kernelSize//2) # creates a list of the image's max coordinates to stop smoothing at
+    b += 1
 
-# for i in range(0,len(imageExt),2): # 0 to  by twos (gets all even numbers for x y z min coords)
-#     imageDimMin[i] = imageExt[i]+1
-# for i in range(1,(len(imageExt)+1),2): # 1 to 5 by twos to get all uneven numbers for x y z max coords
-#     imageDimMax[i] = imageExt[i]-1
+print("image dim min, max= " + str(imageDimMin) + str(imageDimMax))
 
-print(imageDim)
-
-# 3D Gaussian kernel
+# 3D Gaussian kernel, hard coded
+# eventually change this to match the size of the array requested i.e. 3x3, 5x5, etc
 kernel = np.array([[[1, 2, 1], [2, 4, 2], [1, 2, 1]], 
                    [[2, 4, 2], [4, 16, 4], [2, 4, 2]], 
                    [[1, 2, 1], [2, 4, 2], [1, 2, 1]]])
@@ -63,26 +67,48 @@ kernel = np.array([[[1, 2, 1], [2, 4, 2], [1, 2, 1]],
 # kernelNormal normalizes each entry in the array. If all normalized entries are summed, the answer is 1.0
 kernelNormal = kernel/np.sum(kernel)
 
+# Set up a numpy array to fill with image data. 
+# Will use this to get smoothed, filtered values.
+# imageArray = np.empty((imageDim[0], imageDim[1], imageDim[2]), dtype=float)
 
+""" need to fill the imageArray with values from inputImage"""
 
-
-    
-for x in range(0, image.GetDimensions()[0]):
-    for y in range(0, image.GetDimensions()[1]):
-        for z in range(0, image.GetDimensions()[2]):
+# Itereate over all admissiblie pixels in a given image    
+for x in range(imageDimMin[0], imageDimMax[0]):#image.GetDimensions()[0]):
+    for y in range(imageDimMin[1], imageDimMax[1]):#image.GetDimensions()[1]):
+        for z in range(imageDimMin[2], imageDimMax[2]):#image.GetDimensions()[2]):
             
+            # subArray = an extracted value from the imageArray, should be 3x3x3 like the kernel
+            subArray = np.empty((kernelSize, kernelSize, kernelSize), dtype=float)
             
-            # gives the value of the central voxel by summing all the values from the kernelNormal*imageArray convolution
+            # iterate through values to append to the subArray based on 3x3x3 filter
+            # can change the numbers so it isn't hard coded
+            l=0 # dummy i index value
+            for i in range(x-1, x+2):
+                m=0 # dummy j index value
+                l+=1
+                for j in range(y-1, y+2):
+                    n=0 # dummy k index value
+                    m+=1
+                    for k in range(z-1, z+2):
+                        n+=1
+                        voxelValue = inputImage.GetScalarComponentAsFloat(i, j, k, 0)
+                        """subArray is a numpy array and needs to be a vtk object to work"""
+                        # subArray.SetScalarComponentAsFloat((l-1), (m-1), (n-1), voxelValue)
+                        subArray[l-1][m-1][n-1] = voxelValue
+            
+            # gives the value of the central voxel by summing all the values from the kernelNormal*subArray convolution
             # this is the blurring or smoothing step
-            voxelBlur = np.sum(kernelNormal*imageArray)
+            voxelBlurred = np.sum(kernelNormal*subArray)
             
-            
-            voxelValue = image.GetScalarComponentAsFloat(x,y,z,0)
-            image.SetScalarComponentFromFloat(x, y, z, 0, voxelValue)
+            # voxelValue = inputImage.GetScalarComponentAsFloat(x,y,z,0) #inputReader or inputImage????
+            copyImage.SetScalarComponentFromFloat(x, y, z, 0, voxelBlurred)
 
-# test = np.array([[[1, 2, 1], [2, 4, 2], [1, 2, 1]], 
-#                    [[2, 4, 2], [4, 16, 4], [2, 4, 2]], 
-#                    [[1, 2, 1], [2, 4, 2], [1, 2, 1]]])
+
+
+# test = np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9]], 
+#                    [[10, 11, 12], [13, 14, 15], [16, 17, 18]], 
+#                    [[21, 22, 23], [24, 25, 26], [27, 28, 29]]])
 
     
     # write the new smoothed image
--- W02_filtering/02_Assignment/W02_assignment_code.py
+++ W02_filtering/02_Assignment/W02_assignment_code.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 28 16:15:40 2020
-
-@author: MattD
-"""
 
 """
 MDSC 689.03
-Assignment #1: How to load and display a 3D medical image
+Assignment #2: Applying Gaussian and Median filters
 
-Inputs: when prompted, a path to a NIFTI image or DICOM image directory
-        these images do NOT need to be in the working directory
+inputs:
+    >>>python codename.py arg_1 arg_2 arg_3
+    arg_1: path to DICOM folder or NIFTI image
+    arg_2: type of filter to apply. Takes "gaussian" or "median"
+    arg_3: Size of kernel to apply. Takes uneven int (i.e. 3 or 5 or 7...)
+        arg_2 not fully implemented. Only input "3"
+    Example:
+        >>>python codename.py headGaussian.nii gaussian 3
+        
+Other inputs: 
+    when prompted, a path to a NIFTI image or DICOM image directory
+    these images do NOT need to be in the working directory
+    Make sure there are no spaces in the path name
 
-This code will display a single set of DICOM or NIFTI medical images independent
-of the Python working directory.
+This code will filter and display a single set of DICOM or NIFTI 
+medical images independent of the Python working directory.
+It will also write a new filtered image as NIFTI file type to the working directory.
 
-It requires only that you have a DICOM directory path containing only one set
-of DICOM images or a single NIFTI image file path.
-
-Last edited on Jan 25 2020
+Last edited on Feb 01 2020
+# -*- coding: utf-8 -*-
 
 @author: MattD
 
@@ -29,14 +33,20 @@ Last edited on Jan 25 2020
 import vtk   
 import os
 import sys
-    
+import numpy as np
+
+# Argument variables to use throughout code
+imageFile = sys.argv[1]
+filterType = str(sys.argv[2])
+kernelSize = int(sys.argv[3])
+  
 # =============================================================================
 # Prompts the user for the file location of the images to read
 # Checks to see if the file or directory is useable
 # Returns the useable path of the file (NIFTI) or directory (DICOM) to use as a reader
 # This allows the location of the file(s) used to be independent of the working directory
 # =============================================================================
-def readImages():
+def readImages(imageFile):
     
     # dummy variable to run the while loop
     find = True
@@ -44,11 +54,12 @@ def readImages():
     # define the variables to determine the location of DICOM directory or NIFTI file
     while find == True:
         
-        imageFile = input("Please input nifti file path or DICOM folder path:\n " )
+        #global imageFile
+       # imageFile = input("Please input nifti file path or DICOM folder path.\n", 
+       #                   "Make sure there are no spaces in the path (inadmissibile character.\n" )
         
-        if (len(sys.argv) == 2):
-            imageFile = sys.argv[1]
-            print("Using " + os.path.splitext(imageFile)[0] + ' as image to read')
+        if os.path.isdir(imageFile):
+            print("Using " + os.path.basename(imageFile) + ' as directory to read from')
         elif os.path.exists(imageFile):
             print("Using " + os.path.splitext(imageFile)[0] + ' as image to read')
         else:
@@ -103,16 +114,12 @@ def displayImages(reader):
     ydim = int(coordBounds[1])
     zdim = int(coordBounds[2])
     
-    # can be used to display information
-    # print(imageData)
-    # print('x dimension: 1-> ' + str(xdim) + ' y dimension: 1->' + str(ydim) +' number of slices: ' + str(zdim))  
-     
     # initialise the mapper. The image displayed will be the middle image in the image set
     mapper = vtk.vtkImageMapper()
     mapper.SetInputConnection(reader.GetOutputPort())
     mapper.SetZSlice(int(zdim/2))
     mapper.SetColorWindow(1000)
-    mapper.SetColorLevel(200)
+    mapper.SetColorLevel(600)
     
     # create the actor
     actor = vtk.vtkActor2D()
@@ -196,55 +203,136 @@ def displayImages(reader):
     # Start the event loop for the interactor
     iren.Start()
     
-    
     return()
 
-# apply a 3D gaussian filter to an image.
-# input image (inputReader) is of type NIFTIImageReader or DICOMImageReader
-# or possibly just an image????
-def gaussSmooth(inputReader):
+# =============================================================================
+# Apply a smoothing or blurring filter to a desired image.
+# Input is a vtk reader initialized medical image of type DICOM or NIFTI
+# Outputs an updated vtk reader object and writes a new filtered image as NIFTI filetype
+# =============================================================================
+def filterSmooth(inputReader):
     
-    imageName = inputReader.GetFileName()
-    outfile = str(os.path.splitext(imageName)[0]) + '_GaussianSmoothed.nii'
-    outpath = os.path.join(os.getcwd(), outfile)
+    inputImage = inputReader.GetOutput()
     
-    print('\nOutfile:  ' + outfile)
-    print('\nOutpath:  ' + outpath)
+    imagePath = imageFile
+    
+    # determes the filename for the output file based on filter type and input
+    if (os.path.isdir(imagePath)) and filterType == "gaussian":
+#        outfile = "Thorax_GaussianSmoothed"
+        outfile = str(os.path.basename(imagePath)) + '_GaussianSmoothed.nii'
+    elif (os.path.isdir(imagePath)) and filterType == "median":
+#        outfile = "Thorax_MedianSmoothed"
+        outfile = str(os.path.basename(imagePath)) + '_MedianSmoothed.nii'
+    elif filterType == "gaussian":
+        outfile = str(os.path.splitext(imagePath)[0]) + '_GaussianSmoothed.nii'
+    elif filterType == "median":
+        outfile = str(os.path.splitext(imagePath)[0]) + '_MedianSmoothed.nii'
+        
+    ## used for debugging
+    # outpath = os.path.join(os.getcwd(), outfile)
+    # print('\nOutfile:  ' + outfile)
+    # print('\nOutpath:  ' + outpath + '\n')
     
     # make a copy of the input image(s) to work with and maintain original data
     copyImage = vtk.vtkImageData()
     copyImage.DeepCopy(inputReader.GetOutput()) 
     
-    # apply the kernel to smooth the image
+    # get the image dimensions
+    imageDim = inputImage.GetDimensions()
+    imageExt = inputImage.GetExtent()
+
+    kernelSize = 3 # could make this an input arg in the future
     
-    # # 3D Gaussian kernel # do I have to divide by the sum of the matrix elements to normalize? i.e. 16
-    # kernel = [[[1, 2, 1], [2, 4, 2], [1, 2, 1]], 
-    #           [[2, 4, 2], [4, 16, 4], [2, 4, 2]], 
-    #           [[1, 2, 1], [2, 4, 2], [1, 2, 1]]]
+    imageDimMin = np.empty(3, dtype = int) # will have the starting coordinates for the image we want to smooth (padded)
+    imageDimMax = np.empty(3, dtype = int) # will have the ending coordinates for the images we want to smooth (padded)
     
+    ## used for debugging
+    # print("imagedim= " + str(imageDim))
+    # print("image extents= " + str(imageExt))
+    # print("length of image ext= " + str(len(imageExt)))
     
-    # write the new smoothed image
-    # do I need an intermediate step to save all the filtered data in memory as it's being made before writing it?
-    # do I want to save a new 'physical' file, or just keep the information in memore to display?
+    # dummy variables
+    a = 0
+    b = 0
     
-    # smoothImage = vtk.vtkWriter
+    # set up the dimenions of the image we are analyzing to help iterate over the image's data
+    # accounts for the size of the smoothing kernel (i.e. implementing padding)
+    for i in range(0,len(imageExt),2): # 0 to 4 by twos to get all even numbers, corresponds to x y z min coords   
+        imageDimMin[a] = imageExt[i]+(kernelSize - kernelSize//2) # creates a list of the image's min coordinates to start smoothing from
+        a += 1
+    for i in range(1,(len(imageExt)+1),2): # 1 to 5 by twos to get all uneven numbers, corresponds to x y z max coords
+        imageDimMax[b] = imageExt[i]-(kernelSize - kernelSize//2) # creates a list of the image's max coordinates to stop smoothing at
+        b += 1
     
-    """
-    #check code below for voxel navigation and value modification
-    for x in range(0, image.GetDimensions()[0]):
-        for y in range(0, image.GetDimensions()[1]):
-            for z in range(0, image.GetDimensions()[2]):
-                voxelValue = image.GetScalarComponentAsFloat(x,y,z,0)
-                image.SetScalarComponentFromFloat(x, y, z, 0, voxelValue)
+    ## used for debugging
+    # print("image dim min, max= " + str(imageDimMin) + str(imageDimMax))
     
-    """
+    # 3D Gaussian kernel, hard coded. Eventually change this to match the size of the array requested i.e. 3x3, 5x5, etc
+    kernel = np.array([[[1, 2, 1], [2, 4, 2], [1, 2, 1]], 
+                       [[2, 4, 2], [4, 16, 4], [2, 4, 2]], 
+                       [[1, 2, 1], [2, 4, 2], [1, 2, 1]]])
     
-    return()
+    # kernelNormal normalizes each entry in the array. If all normalized entries are summed, the answer is 1.0
+    # will be used as the gaussian kernel for fitering
+    kernelNormal = kernel/np.sum(kernel)
+
+    # used for testing to speed up processing. Set start and end Z-Slices to filter.
+    imageDimMin[2] = 120
+    imageDimMax[2] = 130
+    
+    # Itereate over all admissiblie pixels in a given image    
+    for x in range(imageDimMin[0]-1, imageDimMax[0]+1):
+        for y in range(imageDimMin[1]-1, imageDimMax[1]+1):
+            for z in range(imageDimMin[2]-1, imageDimMax[2]):
+                
+                # a sub array of the image being filtered. Should be the size of the kernel
+                subArray = np.empty((kernelSize, kernelSize, kernelSize), dtype=float)
+                
+                # iterate through values to append to the subArray based on 3x3x3 filter
+                # the created sub array is composed to original image values and will be used to
+                # apply the kernel to, and determine the filtered voxel value(s)
+                l=0 # dummy i index value
+                for i in range(x-1, x+2):
+                    m=0 # dummy j index value
+                    l+=1
+                    for j in range(y-1, y+2):
+                        n=0 # dummy k index value
+                        m+=1
+                        for k in range(z-1, z+2):
+                            n+=1
+                            voxelValue = inputImage.GetScalarComponentAsFloat(i, j, k, 0)
+                            subArray[l-1][m-1][n-1] = voxelValue
+                
+                if filterType == "gaussian":
+                    # gives the value of the central voxel by summing all the values from the kernelNormal*subArray convolution
+                    # this is the gaussian blurring or smoothing step
+                    voxelBlurred = np.sum(kernelNormal*subArray)
+                    copyImage.SetScalarComponentFromFloat(x, y, z, 0, voxelBlurred)
+                elif filterType == "median":
+                    # gives the value of the central voxel by determining the median value of the subArray
+                    # this is the median blurring or smoothing step
+                    voxelMedian = np.median(subArray)
+                    copyImage.SetScalarComponentFromFloat(x, y, z, 0, voxelMedian)
+                
+               # print('. ')
+
+    # write the new filtered image as a NIFTI filetype to the working directory
+    # I'm not sure how to do this for DICOM
+    writer = vtk.vtkNIFTIImageWriter()
+    writer.SetFileName(outfile)
+    writer.SetInputData(copyImage)
+    writer.Write()
+    
+    # updates the vtk reader to the new filtered image
+    smoothedImage = vtk.vtkNIFTIImageReader()
+    smoothedImage.SetFileName(outfile)
+    smoothedImage.Update()
+    
+    return(smoothedImage)
 
 
-reader = readImages()
+reader = readImages(imageFile)
+readerSmoothed = filterSmooth(reader)
 
-gaussSmooth(reader)
-
-displayImages(reader)
+displayImages(readerSmoothed)
     
